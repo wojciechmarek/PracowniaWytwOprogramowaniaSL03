3f54d0a98ad5fdac850a76500e6002deb1d2e184
Wojciech Marek <marek.wojciech@outlook.com>
2021-03-21 10:21:17 +0100

    Add car's properties implementation

--- Lab1/Car/Car.cs
+++ Lab1/Car/Car.cs
@@ -5,9 +5,17 @@ namespace Car
 {
     public class Car
     {
+        public Color Color { get; }
+        public string Brand { get; }
+        public float FuelConsumption { get; }
+        public int TankCapacity { get; }
+
         public Car(Color color, string brand, float fuelConsumption, int tankCapacity)
         {
-            
+            Color = color;
+            Brand = brand;
+            FuelConsumption = fuelConsumption;
+            TankCapacity = tankCapacity;
         }
     }
 }
